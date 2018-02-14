026f065260328ec182aa7bebabe855c8fbe0c0c9
Ana Ausek <anac_ausek@hotmail.com>
2018-02-13 20:33:50 -0500

    Working on app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
-window
-////
+window.onload = launch();
 
+const songs = ["Filthy", "Deer In Headlights", "All Night"];
 
+function displaySongs() {
+    const song = this.song.attr("data-name");
+    const queryURL = "https://api.spotify.com/v1/search?q=" + song + "&type=track&market=US&limit=10&offset=5";
 
-
-
+    
+}
